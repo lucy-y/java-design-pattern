6c0ca87f0593b4ba022b523af38b19d1d80640be
lucy <loveyou_06@naver.com>
2020-04-19 18:15:36 +0900

    add adapter example 2

--- AdapterPattern/src/AdapterImpl.java
+++ AdapterPattern/src/AdapterImpl.java
@@ -3,11 +3,13 @@ public class AdapterImpl implements Adapter{
     //Math math;
     @Override
     public Float twiceOf(Float f){
-        return (float) Math.twoTime(f.doubleValue());
+        System.err.println("call twiceOf");
+        return Math.doubled(f.doubleValue()).floatValue();
     }
 
     @Override
     public Float halfOf(Float f){
+        System.err.println("call half");
         return (float) Math.half(f.doubleValue());
     }
 
